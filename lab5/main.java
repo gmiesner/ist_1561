b2708fd77c63243c0270cc2441462f0dadc4e85a
Gabriella  Miesner <ella.miesner@gmail.com>
2021-03-05 18:52:38 +0000

    lab 5

---
+++ lab5/main.java
@@ -0,0 +1,11 @@
+class Main {
+   static void charToInteger(char one, char two, char three) {
+    System.out.println("My first input is '" + one +"', the output is:\n" + (int) one);
+    System.out.println("My second input is '" + two +"', the output is:\n" + (int) two);
+    System.out.println("My third input is '" + three +"', the output is:\n" + (int) three);
+    
+  }
+  public static void main(String[] args) {
+  charToInteger('a', '%', 'A');
+  }
+}
